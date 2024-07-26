ff8d14d3c7bb1b8f63bfbca76c4744cf828dc0df
Denis Peshkov <denpeshkov98@gmail.com>
2024-07-26 20:07:50 +0400

    refactor doc

--- misc/major.go
+++ misc/major.go
@@ -7,10 +7,10 @@ import (
 // FindMajor finds the majority element in a sequence of elements.
 // It returns the majority element if one exists; otherwise, it returns an arbitrary element.
 // It is a streaming algorithm requiring only a single pass through the iterator.
-//
 // It uses Boyer–Moore majority vote streaming algorithm.
-//   - Time: O(N)
-//   - Space: O(1)
+//
+// Time: O(N)
+// Space: O(1)
 func FindMajor[T comparable](seq iter.Seq[T]) T {
 	var (
 		m T
